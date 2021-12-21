9da439be276231708bf8abc14a0a5ae546dcf07f
QemerS <tu201806169@code.edu.az>
2021-12-21 06:07:22 +0400

    making connection

--- SixteenClothingTemplate/SixteenClothingTemplate/Data/AppDbContext.cs
+++ SixteenClothingTemplate/SixteenClothingTemplate/Data/AppDbContext.cs
@@ -1,11 +1,21 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using SixteenClothingTemplate.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SixteenClothingTemplate.Data
 {
-    public class AppDbContext
+    public class AppDbContext: DbContext
     {
+        public AppDbContext(DbContextOptions options): base(options)
+        { }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Setting> Settings { get; set; }
+        public DbSet<Social> Socials { get; set; }
+        public DbSet<Speciality> Specialities { get; set; }
+        public DbSet<Team> Teams { get; set; }
     }
 }

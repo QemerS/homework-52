2246f6c6f15933d1b6e02f9d4523ed32cb42b171
QemerS <tu201806169@code.edu.az>
2021-12-27 14:06:19 +0400

    Partial tetbiqi

---
+++ SixteenClothingTemplate/SixteenClothingTemplate/Models/Partial_models.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SixteenClothingTemplate.Models
+{
+    public class Partial_models
+    {
+        public string Red_one { get; set; }
+        public string Big_one { get; set; }
+    }
+}
